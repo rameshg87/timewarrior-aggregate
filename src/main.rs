27c1c83ab44746722e300e4adee6109c9721e3d2
Ramakrishnan G <rameshg87@gmail.com>
2021-07-26 17:54:36 +0530

    Improve error messages

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,48 @@
 // ~/.timewarrior/aggregate directory. The tool is supposed to be helpful in identifying the
 // various things required to understand how to use it.
 
+use crate::twinput::TimeWarriorInput;
+use isatty::stdin_isatty;
+use std::env;
 use std::io::{self, Read};
+use std::path::PathBuf;
 
 pub mod tagset;
 pub mod twentry;
 pub mod twinput;
 pub mod workgroup;
 
-use crate::twinput::TimeWarriorInput;
+fn check_exe() {
+    let current_exe = std::env::current_exe().unwrap();
+    let mut expected_exe = PathBuf::new();
+    expected_exe.push(env::var("HOME").unwrap());
+    expected_exe.push(".timewarrior");
+    expected_exe.push("extensions");
+    expected_exe.push("aggregate");
+    if current_exe != expected_exe {
+        let expected_exe = expected_exe.as_path().to_str().unwrap();
+        println!(
+            "Install this binary in {} inorder for it to be an extension of timewarrior.",
+            expected_exe
+        );
+        std::process::exit(1);
+    }
+}
+
+fn check_stdin_isatty() {
+    if stdin_isatty() {
+        println!("This binary is supposed to be invoked by timewarrior.");
+        println!("Run 'timew aggregate :day' or 'timew aggregate :week' to get started.");
+        std::process::exit(1);
+    }
+}
 
 fn main() {
     // Check if ~/.timewarrior/aggregate directory exists.
     env_logger::init();
+
+    check_exe();
+    check_stdin_isatty();
 
     // Accept the standard input and retrieve the individual items
     let mut buffer = String::new();
--- src/twinput.rs
+++ src/twinput.rs
@@ -1,7 +1,5 @@
 use crate::twentry::TimeWarriorEntry;
 use log::debug;
-use std::env;
-use std::path::PathBuf;
 
 pub struct TimeWarriorInput {
     pub start: String,
@@ -35,19 +33,6 @@ impl TimeWarriorInput {
             }
         }
         if start.len() == 0 || end.len() == 0 {
-            let current_exe = std::env::current_exe().unwrap();
-            let mut expected_exe = PathBuf::new();
-            expected_exe.push(env::var("HOME").unwrap());
-            expected_exe.push(".timewarrior");
-            expected_exe.push("extensions");
-            expected_exe.push("aggregate");
-            if current_exe != expected_exe {
-                let current_exe = current_exe.as_path().to_str().unwrap();
-                let expected_exe = expected_exe.as_path().to_str().unwrap();
-                return Err(
-                    format!("This binary is supposed to be installed in {} inorder for it to be an extension of timewarrior.\nCurrently it was executed from {}\n", expected_exe, current_exe)
-                );
-            }
             return Err(
                 "Unable to find timewarrior passed statistics in standard input.\nWas this program run directly? This program is supposed to be invoked by timewarrior.\n".to_string()
             );
